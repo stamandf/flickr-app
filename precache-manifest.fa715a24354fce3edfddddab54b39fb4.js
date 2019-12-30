5941e939cab537e5f50a27c9ad8683f37bdeef94
Florence <stamandf@gmail.com>
2019-12-30 00:30:32 -0500

    Updates

---
+++ precache-manifest.fa715a24354fce3edfddddab54b39fb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0ac9e525c3fb54a7f08f4a593cc260",
+    "url": "/flickr-app/index.html"
+  },
+  {
+    "revision": "c36ccda2e10a7df4cc57",
+    "url": "/flickr-app/static/css/main.e435ae02.chunk.css"
+  },
+  {
+    "revision": "71f93104a1adc4bb0b10",
+    "url": "/flickr-app/static/js/2.06da6076.chunk.js"
+  },
+  {
+    "revision": "c36ccda2e10a7df4cc57",
+    "url": "/flickr-app/static/js/main.e707529c.chunk.js"
+  },
+  {
+    "revision": "5ea84d430db815c4a28c",
+    "url": "/flickr-app/static/js/runtime-main.bf5ff01d.js"
+  }
+]);
