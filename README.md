612f779407ec360823e5a0a4e2678a95c009cdcf
Florence <stamandf@gmail.com>
2019-12-30 02:41:20 -0500

    Modify readme info.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-Create shareable walls of pictures based on keyword search query
-Use Flickr API
+# Coding Challenge
+Fork this repository, complete challenge, submit pull request and provide product preview URL.
 
+### Project Guidelines
+* Create shareable walls of pictures based on keyword search query
+* Use [Flickr API](https://www.flickr.com/services/api)
+
+### Technical Guidelines
+* Javascript Framework
+* Responsive
+
+### Product Specifications
+* See wireframe.png
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97a8312d24ffda9ec8ed21c2f87c5729.js"
+  "/flickr-app/precache-manifest.fa715a24354fce3edfddddab54b39fb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flickr-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
