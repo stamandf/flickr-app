a799c4c65513e4b1c1bc400530dd81f05e86e967
Florence <stamandf@gmail.com>
2020-01-04 12:41:09 -0500

    Add flickr constants in a separate file.

---
+++ src/utilities/flickrConstants.js
@@ -0,0 +1,13 @@
+const KEY = '&api_key=44779de00e72e9ec8ffbf165bc0f6ca6';
+const FLICKRMETHOD = 'https://www.flickr.com/services/rest/?method=flickr.photos.search';
+const FORMAT = '&format=json&nojsoncallback=1';
+
+console.log(`KEY=${KEY}, FLICKRMETHOD=${FLICKRMETHOD}, FORMAT=${FORMAT}`);
+
+// module.exports = {
+//     KEY : KEY,
+//     FLICKRMETHOD: FLICKRMETHOD,
+//     FORMAT: FORMAT
+// }
+
+module.exports = { KEY,FLICKRMETHOD, FORMAT }
--- src/utilities/getPhotos.js
+++ src/utilities/getPhotos.js
@@ -1,14 +1,19 @@
+const flickr = require('./flickrConstants');
 const request = require('request');
 
+console.log('Flickr constants are:', flickr.KEY, flickr.FLICKRMETHOD, flickr.FORMAT);
+
 const fetchPhotos = (srchtext, srchpage, callback) => {
-    const key = '&api_key=44779de00e72e9ec8ffbf165bc0f6ca6';
-    const flickrMethod = 'https://www.flickr.com/services/rest/?method=flickr.photos.search';
-    const format = '&format=json&nojsoncallback=1';
+    // const key = '&api_key=44779de00e72e9ec8ffbf165bc0f6ca6';
+    // const flickrMethod = 'https://www.flickr.com/services/rest/?method=flickr.photos.search';
+    // const format = '&format=json&nojsoncallback=1';
     let text = '&text=' + srchtext;
     let page = '&page=' + srchpage;
 
     // The flickr method flickr.photos.search request URL
-    let req_url = flickrMethod + key + text + page + format;
+    // let req_url = flickrMethod + key + text + page + format;
+    let req_url = flickr.FLICKRMETHOD + flickr.KEY + text + page + flickr.FORMAT;
+
 
     request ({
         url: req_url,
